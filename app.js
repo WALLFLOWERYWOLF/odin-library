231184cdb6814b6259082b9fffbdeb84cc608cf7
WALLFLOWERYWOLF <121654472+WALLFLOWERYWOLF@users.noreply.github.com>
2026-08-31 09:32:36 +0530

    fix: reset form fields on modal cancel

--- app.js
+++ app.js
@@ -1,9 +1,13 @@
 const addBookBtn = document.querySelector(".add-book-btn");
 const dialog = document.querySelector(".dialog");
 const cancel = document.querySelector(".cancel");
+const form = document.querySelector("#form");
+
 addBookBtn.addEventListener("click", () => {
   dialog.showModal();
 });
+
 cancel.addEventListener("click", () => {
   dialog.close();
+  form.reset();
 });
